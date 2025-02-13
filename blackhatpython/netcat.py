6dd42cb9e895dd4fe9b3d719b216f9b27a2412d0
trop3n <jasondkimm@proton.me>
2025-02-12 23:54:28 -0600

    added NetCat class, added send function

--- blackhatpython/netcat.py
+++ blackhatpython/netcat.py
@@ -14,10 +14,46 @@ def execute(cmd):
     output = subprocess.check_output(shlex.split(cmd)), stderr=subprocess.STDOUT)
     return output.decode()
 
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User Terminated.')
+            self.socket.close()
+            sys.exit()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='BHP Net Tool',
-        formatter_class=arg_parse.RawDescriptionHelpFormatter,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''Example:
             netcat.py -t 192.168.1.108 -p 5555 -l -c #command shell
             netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
