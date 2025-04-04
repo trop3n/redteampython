f307da21b4d642e2ea5613c91baf324e265b4c8e
trop3n <jasondkimm@proton.me>
2025-04-03 22:38:08 -0500

    added ssh_server.py and imported libraries

---
+++ blackhatpython/ssh_cmd/ssh_server.py
@@ -0,0 +1,12 @@
+import os
+import paramiko
+import socket
+import sys
+import threading
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
+
+class Server (paramiko.ServerInterface):
+    def __init__(self):
+        self.event = threading.Event()
