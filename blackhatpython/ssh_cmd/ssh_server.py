f598263f4825c55dbefb7073d0b8f9b97257a2ea
trop3n <jasondkimm@proton.me>
2025-04-03 22:45:00 -0500

    added __name__ == __main__ and try except block

--- blackhatpython/ssh_cmd/ssh_server.py
+++ blackhatpython/ssh_cmd/ssh_server.py
@@ -5,9 +5,9 @@ import sys
 import threading
 
 CWD = os.path.dirname(os.path.realpath(__file__))
-HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key')) # 1
 
-class Server (paramiko.ServerInterface):
+class Server (paramiko.ServerInterface): # 2
     def __init__(self):
         self.event = threading.Event()
 
@@ -19,3 +19,17 @@ class Server (paramiko.ServerInterface):
     def check_auth_password(self, username, password):
         if (username == 'tim') and (password == 'sekret'):
             return paramiko.AUTH_SUCCESSFUL
+    
+if __name__ == '__main__':
+    server = '192.168.1.207'
+    ssh_port = 2222
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((server, ssh_port)) # 3
+        sock.listen(100)
+        print('[+] Listening for connection ...')
+        client, addr = sock.accept()
+    except Exception as e:
+        print('[-] Listen failed: ' + str(e))
+        sys.exit(1)
