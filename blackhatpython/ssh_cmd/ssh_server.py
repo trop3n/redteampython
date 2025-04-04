67371f0390ad29685b55f5673af4f14e50949c25
trop3n <jasondkimm@proton.me>
2025-04-03 22:41:57 -0500

    added class and functions for paramiko.ServerInterface

--- blackhatpython/ssh_cmd/ssh_server.py
+++ blackhatpython/ssh_cmd/ssh_server.py
@@ -10,3 +10,12 @@ HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
 class Server (paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+    
+    def check_auth_password(self, username, password):
+        if (username == 'tim') and (password == 'sekret'):
+            return paramiko.AUTH_SUCCESSFUL
