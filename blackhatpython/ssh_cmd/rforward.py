2440837e0067aa2aae32bac1966638bf57e6e30e
trop3n <jasondkimm@proton.me>
2025-04-04 22:45:04 -0500

    added more code to the rforward example from Paramiko demo files

--- blackhatpython/ssh_cmd/rforward.py
+++ blackhatpython/ssh_cmd/rforward.py
@@ -31,3 +31,30 @@ def main():
         except KeyboardInterrupt:
             print('C-c: Port forwarding stopped.')
             sys.exit(0)
+
+# ---- SNIP ---- #
+def handler(chan, host, port):
+    sock = socket.socket()
+    try:
+        sock.connect((host, port))
+    except Exception as e:
+        verbose('Forwarding request to %s:%d failed: %r' % (host, port, e))
+        return
+    
+    verbose('Connected! Tunnel open %r -> %r -> %r' % (chan.origin_addr, chan.getpeername(), (host, port))
+    )
+    while True:
+        r, w, x = select.select([sock, chan], [], [])
+        if sock in r:
+            data = sock.recv(1024)
+            if len(data) == 0:
+                break
+            chan.send(data)
+        if chan in r:
+            data = chan.recv(1024)
+            if len(data) == 0:
+                break
+            sock.send(data)
+    chan.close()
+    sock.close()
+    verbose('Tunnel closed from %r' % (chan.origin_addr))
